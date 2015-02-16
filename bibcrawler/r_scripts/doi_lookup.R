1185987a2efcb484e23a91edcc7b1797e04ccaea
Asura Enkhbayar <asura.enkhbayar@gmail.com>
2015-02-16 19:34:07 +0100

    Changed input argument types from list to str

--- bibcrawler/r_scripts/doi_lookup.R
+++ bibcrawler/r_scripts/doi_lookup.R
@@ -1,29 +1,17 @@
 library(rcrossref)
 
-crossref <- function(authors, titles, submission){
-  for (i in 1:length(authors)){
-    print(paste(i,"------------"))
-    
-    title = gsub("\r|\n|\r\n", "", titles[i])
-    title = gsub(" +", "+",title)
-    author = gsub("\\. +|\\|| +","+",authors[i])
-    sub_date = strptime(submission[i], format="%Y-%m-%d %H:%M:%S")$year+1900
-    
-    query_string = paste(title,author,sep="+")
-    query_string = gsub("[^a-zA-Z0-9+ -]","",query_string)
-    print(query_string)
-    
-    # Currently filtering a window of +/- 1 year around the submission date.
-    r = cr_works(query=query_string,
-                 filter=c(from_pub_date=sub_date-1, until_pub_date=sub_date+1), limit=1)
-    print(r$meta)
-    
-    if (i==1){
-      results = r$data
-    } else {
-      results = rbind.fill(r$data,results)
-    }  
-  }
+crossref <- function(author, title, submission){
+  title = gsub("\r|\n|\r\n", "", title)
+  title = gsub(" +", "+",title)
+  author = gsub("\\. +|\\|| +","+",author)
+  sub_date = strptime(submission, format="%Y-%m-%d %H:%M:%S")$year+1900
   
-  return(results)
+  query_string = paste(title,author,sep="+")
+  query_string = gsub("[^a-zA-Z0-9+ -]","",query_string)
+  print(query_string)
+  
+  # Currently filtering a window of +/- 1 year around the submission date.
+  r = cr_works(query=query_string,
+               filter=c(from_pub_date=sub_date-1, until_pub_date=sub_date+1), limit=1)
+  return(r$data)
 }
