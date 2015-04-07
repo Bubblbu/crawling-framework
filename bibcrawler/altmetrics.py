54589eab44e37c2edc680fcb5f2e7e6cd0e592db
Asura Enkhbayar <asura.enkhbayar@gmail.com>
2015-04-07 15:38:57 +0200

    Mendeley document pipeline - status WebSci15

--- bibcrawler/altmetrics.py
+++ bibcrawler/altmetrics.py
@@ -23,60 +23,112 @@ def start_mendeley_session(mndly_config):
     return auth.authenticate()
 
 
+def init_temp(src):
+    temp = dict()
+    temp['arxiv_id'] = src['arxiv_id']
+    temp['arxiv_doi'] = src['arxiv_doi']
+    temp['crossref_doi'] = src['crossref_doi']
+    temp['mndly_path'] = src['mndly_path']
+    temp['submitted'] = src['submitted']
+
+    temp['mndly_match'] = np.nan
+    temp['type'] = np.nan
+    temp['identifiers'] = np.nan
+    temp['keywords'] = np.nan
+    temp['abstract'] = np.nan
+    temp['link'] = np.nan
+    temp['authors'] = np.nan
+
+    # bibliometic data
+    temp['pages'] = np.nan
+    temp['volume'] = np.nan
+    temp['issue'] = np.nan
+    temp['websites'] = np.nan
+    temp['month'] = np.nan
+    temp['publisher'] = np.nan
+    temp['day'] = np.nan
+    temp['city'] = np.nan
+    temp['edition'] = np.nan
+    temp['institution'] = np.nan
+    temp['series'] = np.nan
+    temp['chapter'] = np.nan
+    temp['revision'] = np.nan
+    temp['editors'] = np.nan
+
+    # readership statistics
+    temp['reader_count'] = np.nan
+    temp['reader_count_by_academic_status'] = np.nan
+    temp['reader_count_by_subdiscipline'] = np.nan
+    temp['reader_count_by_country'] = np.nan
+
+    return temp
+
+
 def add_new_entry(list_of_dicts, src, mndly_doc):
+    temp = init_temp(src)
+
     # Check with arXiv data
-    if 'arxiv' in mndly_doc.identifiers:
-        arxiv_id = src['arxiv_id']
-        mndly_arxiv_id = mndly_doc.identifiers['arxiv']
-        found_regex = new_arxiv_format.findall(mndly_doc.identifiers['arxiv'])
-        if found_regex:
-            mndly_arxiv_id = found_regex[0]
-        else:
-            found_regex = old_arxiv_format.findall(mndly_doc.identifiers['arxiv'])
+    if not mndly_doc:
+        pass
+    else:
+        if 'arxiv' in mndly_doc.identifiers:
+            arxiv_id = src['arxiv_id']
+            mndly_arxiv_id = mndly_doc.identifiers['arxiv']
+
+            # TODO - remove the version number
+            found_regex = new_arxiv_format.findall(mndly_doc.identifiers['arxiv'])
             if found_regex:
                 mndly_arxiv_id = found_regex[0]
+            else:
+                found_regex = old_arxiv_format.findall(mndly_doc.identifiers['arxiv'])
+                if found_regex:
+                    mndly_arxiv_id = found_regex[0]
 
-        found_regex = new_arxiv_format.findall(src['arxiv_id'])
-        if found_regex:
-            arxiv_id = found_regex[0]
-        else:
-            found_regex = old_arxiv_format.findall(mndly_doc.identifiers['arxiv'])
+            found_regex = new_arxiv_format.findall(src['arxiv_id'])
             if found_regex:
                 arxiv_id = found_regex[0]
+            else:
+                found_regex = old_arxiv_format.findall(mndly_doc.identifiers['arxiv'])
+                if found_regex:
+                    arxiv_id = found_regex[0]
 
-        if arxiv_id == mndly_arxiv_id:
-            print("=== GOOD ===")
-            print(arxiv_id, " - ", mndly_arxiv_id)
-            print(src['title'], " - ", mndly_doc.title)
-            check = True
+            if arxiv_id == mndly_arxiv_id:
+                print("\t\tDocuments id's match")
+                print("\t\t " + arxiv_id, " - ", mndly_arxiv_id)
+                print("\t\t " + src['title'], " - ", mndly_doc.title)
+                check = True
+            else:
+                print("\t\tId's Mismatch")
+                print("\t\t " + arxiv_id, " - ", mndly_arxiv_id)
+                print("\t\t " + src['title'], " - ", mndly_doc.title)
+                check = False
         else:
-            print("=== BAD ===")
-            print(arxiv_id, " - ", mndly_arxiv_id)
-            print(src['title'], " - ", mndly_doc.title)
-            check = False
+            lr = levenshtein_ratio(src['title'], mndly_doc.title)
+            if lr <= LR:
+                print("\t\tDocuments titles match - {}".format(lr))
+                print("\t\t " + src['title'], " - ", mndly_doc.title)
+                check = True
+            else:
+                print("\t\tTitles Mismatch - {}".format(lr))
+                print("\t\t " + src['title'], " - ", mndly_doc.title)
+                check = False
 
-    else:
-        lr = levenshtein_ratio(src['title'], mndly_doc.title)
-        if lr <= LR:
-            print("=== GOOD === {}".format(lr))
-            print(src['title'], " - ", mndly_doc.title)
-            check = True
+        if check:
+            temp['mndly_match'] = True
         else:
-            print("=== BAD === {}".format(lr))
-            print(src['title'], " - ", mndly_doc.title)
-            check = False
-
-    temp = dict()
-    if check:
-        temp['arxiv_id'] = src['arxiv_id']
-        temp['arxiv_doi'] = src['arxiv_doi']
-        temp['crossref_doi'] = src['crossref_doi']
-        temp['mndly_found'] = True
-        temp['mndly_path'] = src['mndly_path']
-        temp['mndly_kicked'] = np.nan
+            temp['mndly_match'] = False
 
         temp['type'] = mndly_doc.type
-        temp['identifiers'] = mndly_doc.identifiers
+        temp['identifier_count'] = len(mndly_doc.identifiers)
+        temp['identifiers'] = "-".join(mndly_doc.identifiers)
+
+        temp['mndly_arxiv'] = mndly_doc.identifiers['arxiv'] if 'arxiv' in mndly_doc.identifiers else np.nan
+        temp['mndly_doi'] = mndly_doc.identifiers['doi'] if 'doi' in mndly_doc.identifiers else np.nan
+        temp['mndly_issn'] = mndly_doc.identifiers['issn'] if 'issn' in mndly_doc.identifiers else np.nan
+        temp['mndly_isbn'] = mndly_doc.identifiers['isbn'] if 'isbn' in mndly_doc.identifiers else np.nan
+        temp['mndly_scopus'] = mndly_doc.identifiers['scopus'] if 'scopus' in mndly_doc.identifiers else np.nan
+        temp['mndly_pmid'] = mndly_doc.identifiers['pmid'] if 'pmid' in mndly_doc.identifiers else np.nan
+
         temp['keywords'] = mndly_doc.keywords
         temp['abstract'] = mndly_doc.abstract
         temp['link'] = mndly_doc.link
@@ -111,61 +163,16 @@ def add_new_entry(list_of_dicts, src, mndly_doc):
         temp['reader_count_by_academic_status'] = mndly_doc.reader_count_by_academic_status
         temp['reader_count_by_subdiscipline'] = mndly_doc.reader_count_by_subdiscipline
         temp['reader_count_by_country'] = mndly_doc.reader_count_by_country
+        # else:
+        #     if 'arxiv' in mndly_doc.identifiers:
+        #         ret_val = (src['arxiv_id'], mndly_doc.identifiers['arxiv'])
+        #     else:
+        #         ret_val = (unicode(src['title']).encode('utf-8'), unicode(mndly_doc.title).encode('utf-8'))
+        #
+        #     temp['mndly_kicked'] = ret_val
 
-        list_of_dicts.append(temp)
-
-        return
-
-    else:
-        print("******  FOUND MEND mndly_doc DID NOT MATCH ARXIV   *******")
-        if 'arxiv' in mndly_doc.identifiers:
-            print(src['arxiv_id'] + " - " + mndly_doc.identifiers['arxiv'])
-            ret_val = (src['arxiv_id'], mndly_doc.identifiers['arxiv'])
-
-        else:
-            print(src['title'] + " - " + mndly_doc.title)
-            ret_val = (unicode(src['title']).encode('utf-8'), unicode(mndly_doc.title).encode('utf-8'))
-
-        temp['arxiv_id'] = src['arxiv_id']
-        temp['arxiv_doi'] = src['arxiv_doi']
-        temp['crossref_doi'] = src['crossref_doi']
-        temp['mndly_found'] = False
-        temp['mndly_path'] = src['mndly_path']
-        temp['mndly_kicked'] = ret_val
-
-        temp['type'] = np.nan
-        temp['identifiers'] = np.nan
-        temp['keywords'] = np.nan
-        temp['abstract'] = np.nan
-        temp['link'] = np.nan
-        temp['authors'] = np.nan
-
-
-        # bibliometic data
-        temp['pages'] = np.nan
-        temp['volume'] = np.nan
-        temp['issue'] = np.nan
-        temp['websites'] = np.nan
-        temp['month'] = np.nan
-        temp['publisher'] = np.nan
-        temp['day'] = np.nan
-        temp['city'] = np.nan
-        temp['edition'] = np.nan
-        temp['institution'] = np.nan
-        temp['series'] = np.nan
-        temp['chapter'] = np.nan
-        temp['revision'] = np.nan
-        temp['editors'] = np.nan
-
-        # readership statistics
-        temp['reader_count'] = np.nan
-        temp['reader_count_by_academic_status'] = np.nan
-        temp['reader_count_by_subdiscipline'] = np.nan
-        temp['reader_count_by_country'] = np.nan
-
-        list_of_dicts.append(temp)
-
-        return
+    list_of_dicts.append(temp)
+    return
 
 
 def mendeley_altmetrics(stage1_dir=None, stage2_dir=None):
@@ -187,8 +194,11 @@ def mendeley_altmetrics(stage1_dir=None, stage2_dir=None):
         all_subdirs = [stage1_dir + d for d in os.listdir(stage1_dir) if os.path.isdir(stage1_dir + d)]
         latest_subdir = max(all_subdirs, key=os.path.getmtime)
         stage2_dir = latest_subdir + "/"
+    else:
+        stage2_dir = stage1_dir + stage2_dir + "/"
 
     working_folder = stage2_dir + timestamp
+    print(working_folder)
     os.makedirs(working_folder)
 
     # Read in stage 2 file
@@ -197,9 +207,23 @@ def mendeley_altmetrics(stage1_dir=None, stage2_dir=None):
     input_df = input_df.replace("", np.nan)
 
     output_dicts = []
+    max_len = len(input_df.index)
+    for count, (idx, row) in enumerate(input_df.iterrows()):
+        arxiv_document = None
+        doi_document = None
 
-    for idx, row in input_df.iterrows():
+        print("\n=== FILE {} of {} ===".format(count, max_len))
+
         arxiv_id = row['id']
+        found_regex = new_arxiv_format.findall(arxiv_id)
+        if found_regex:
+            arxiv_id = found_regex[0]
+        else:
+            found_regex = old_arxiv_format.findall(arxiv_id)
+            if found_regex:
+                arxiv_id = found_regex[0]
+        print(arxiv_id)
+
         if doi_check.match(str(row['doi'])):
             doi = row['doi']
         else:
@@ -212,86 +236,69 @@ def mendeley_altmetrics(stage1_dir=None, stage2_dir=None):
                'arxiv_doi': row['doi'],
                'crossref_doi': row['crossref_doi'],
                'title': row['title'],
+               'submitted': row['submitted'],
                'mndly_path': np.nan}
 
         if doi:
-            doi_document = None
             try:
                 doi_document = session.catalog.by_identifier(doi=doi, view='all')
-            except (MendeleyException, MendeleyApiException):
+            except (MendeleyException, MendeleyApiException), e:
                 pass
             except Exception, e:
                 print("HTTP", str(e))
 
-            arxiv_document = None
             try:
                 arxiv_document = session.catalog.by_identifier(arxiv=arxiv_id, view='all')
-            except (MendeleyException, MendeleyApiException):
+            except (MendeleyException, MendeleyApiException), e:
                 pass
             except Exception, e:
                 print("HTTP", str(e))
 
             if doi_document and not arxiv_document:
-                print("\ndoi found, but no arxiv")
+                print("\t+ doi found, but no arxiv")
+                src['mndly_path'] = 1
                 add_new_entry(output_dicts, src, doi_document)
 
             elif not doi_document and arxiv_document:
-                print("\narxiv found, but no doi")
+                print("\t+ arxiv found, but no doi")
+                src['mndly_path'] = 2
                 add_new_entry(output_dicts, src, arxiv_document)
 
             elif not doi_document and not arxiv_document:
-                print("\nNone found")
+                print("\t+ None found")
+                src['mndly_path'] = 7
+                add_new_entry(output_dicts, src, None)
                 continue
 
             else:
-                print("\nboth found")
-                # self.arxiv_doi_found = [id_mndly_doc.arxiv_id, True, True]
-                # self.lookup_doi_and_arxiv_both += 1
-                # if doi_mndly_document.id == arxiv_mndly_document.id:
-                # self.path = 3
-                # self.lookup_doi_and_arxiv_both_match += 1
-                #     ret_val = id_mndly_doc.add_mendeley_data(doi_mndly_document)
-                #     if ret_val[0]:
-                #         self.mndly_docs_after_fuzzy_title_match += 1
-                #         self.mndly_document_queue.put(id_mndly_doc)
-                #
-                #     else:
-                #         self.kicked = ret_val[1]
-                #         self.kicked_doi += 1
-                # else:
-                #     if len(doi_mndly_document.identifiers) >= len(arxiv_mndly_document.identifiers):
-                #         self.path = 4
-                #         self.lookup_doi_and_arxiv_both_doi += 1
-                #         ret_val = id_mndly_doc.add_mendeley_data(doi_mndly_document)
-                #         if ret_val[0]:
-                #             self.mndly_docs_after_fuzzy_title_match += 1
-                #             self.mndly_document_queue.put(id_mndly_doc)
-                #         else:
-                #             self.kicked = ret_val[1]
-                #             self.kicked_doi += 1
-                #     else:
-                #         self.path = 5
-                #         self.lookup_doi_and_arxiv_both_arxiv += 1
-                #         ret_val = id_mndly_doc.add_mendeley_data(arxiv_mndly_document)
-                #         if ret_val[0]:
-                #             self.mndly_docs_after_fuzzy_title_match += 1
-                #             self.mndly_document_queue.put(id_mndly_doc)
-                #         else:
-                #             self.kicked = ret_val[1]
-                #             self.kicked_arxiv += 1
+                print("\t+ both found")
+                if doi_document.id == arxiv_document.id:
+                    src['mndly_path'] = 3
+                    add_new_entry(output_dicts, src, arxiv_document)
+                else:
+                    if len(doi_document.identifiers) >= len(arxiv_document.identifiers):
+                        src['mndly_path'] = 4
+                        add_new_entry(output_dicts, src, doi_document)
+                    else:
+                        src['mndly_path'] = 5
+                        add_new_entry(output_dicts, src, arxiv_document)
         else:
             try:
                 arxiv_document = session.catalog.by_identifier(arxiv=arxiv_id, view='all')
                 if arxiv_document:
-                    print("arxiv found - solo")
+                    print("\t+ arxiv found - solo")
+                    src['mndly_path'] = 6
                     add_new_entry(output_dicts, src, arxiv_document)
-                else:
-                    print("nothing found - solo")
-            except (MendeleyException, MendeleyApiException):
-                print("MendeleyException DAFUCK WHAT WHAT")
+            except (MendeleyException, MendeleyApiException), e:
+                print("\t+ nothing found - solo")
+                src['mndly_path'] = 8
+                add_new_entry(output_dicts, src, None)
                 pass
             except Exception, e:
                 print("HTTP?", str(e))
+
+    output = pd.DataFrame(output_dicts)
+    output.to_json(working_folder + "/stage_3.json")
 
 
 if __name__ == "__main__":
