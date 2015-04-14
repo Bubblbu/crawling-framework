968d6fae9fe11d55ffeacf687d147d9807574b69
Asura Enkhbayar <asura.enkhbayar@gmail.com>
2015-04-14 17:01:12 +0200

    Minor changes

--- bibcrawler/r_scripts/doi_lookup.R
+++ bibcrawler/r_scripts/doi_lookup.R
@@ -4,7 +4,7 @@ crossref <- function(author, title, submission){
   title = gsub("\r|\n|\r\n", "", title)
   title = gsub(" +", "+",title)
   author = gsub("\\. +|\\|| +","+",author)
-  sub_date = strptime(submission, format="%Y-%m-%d %H:%M:%S")$year+1900
+  sub_date = strptime(submission, format="%Y-%m-%d %H:%M:%S")[['year']]+1900
   
   query_string = paste(title,author,sep="+")
   query_string = gsub("[^a-zA-Z0-9+]","",query_string)
@@ -12,5 +12,6 @@ crossref <- function(author, title, submission){
   # Currently filtering a window of +/- 1 year around the submission date.
   r = cr_works(query=query_string,
                filter=c(from_pub_date=sub_date-1, until_pub_date=sub_date+1), limit=1)
-  return(r$data)
+  
+  return(data.frame(r[['data']]))
 }
